e8fcd6b357f77b164df11a3ed794bdc7a73bdab1
vishusharmaX <sharmavishwajeet04@gmail.com>
2023-11-17 19:43:30 +0530

    first commit

---
+++ Practise set1/script.js
@@ -0,0 +1,25 @@
+let a = "Hwllo World"
+
+
+
+const user = [{
+        id: "Dennis",
+        name: "",
+        email: "",
+    },
+    {
+        id: "76",
+        name: ">Carl Chapman",
+        email: "lajla@kak.jo",
+    },
+    {
+        id: "58",
+        name: ">Lucy Shaw",
+        email: "okima@cowe.tg",
+    },
+    {
+        id: "32",
+        name: ">Christian Russell",
+        email: "zij@ve.sy",
+    },
+]
